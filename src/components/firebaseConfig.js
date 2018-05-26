c1ec8f55d248576da201edb20bdee29d7fe1ba8b
Kevin <stradtkt22@gmail.com>
2018-05-26 15:12:44 -0400

    employee manager

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+        apiKey: "AIzaSyCcdy6jNGlYKfadY1b_b4ES-mxqjVJAOKI",
+        authDomain: "vue-js-prod.firebaseapp.com",
+        databaseURL: "https://vue-js-prod.firebaseio.com",
+        projectId: "vue-js-prod",
+        storageBucket: "vue-js-prod.appspot.com",
+        messagingSenderId: "31493995355"
+}
